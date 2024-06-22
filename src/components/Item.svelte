4c4dd9038f78e2e8936986d5cbcc9f5f399c17d8
gummangummangumman <gumman@gumman.one>
2024-06-23 00:01:15 +0200

    item knapp

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import type { Item } from '../types/item';
 	export let item: Item;
+
+	let number: number = 0; //TODO replace with gameState
 </script>
 
 <div class="border border-black">
-	{item.name}
+	<button on:click={() => number++}>Buy {item.name}</button>
 	<br />
-	{item.cost}
+	Cost: <strong>{item.cost}</strong> seeds
+	<br />
+	You have <strong>{number}</strong> {item.name}s
 </div>
