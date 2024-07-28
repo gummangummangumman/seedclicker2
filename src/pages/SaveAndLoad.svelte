7a13b269e78ea867378ad305571af0553707369f
gummangummangumman <gumman@gumman.one>
2024-07-28 11:03:54 +0200

    bedre lagring og lasting

--- src/pages/SaveAndLoad.svelte
+++ src/pages/SaveAndLoad.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
 	import { gameState } from '../store/store';
+
+	let loadFile: HTMLInputElement;
+	let loadText: HTMLTextAreaElement;
+
 	function hardReset() {
 		alert('not implemented yet');
+	}
+
+	function handleFileChange(event: Event) {
+		const input = event.target as HTMLInputElement;
+		if (input.files && input.files.length > 0) {
+			const file = input.files[0];
+
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				loadText.value = reader.result as string;
+			};
+
+			reader.onerror = () => {
+				console.error('Could not read the file');
+			};
+
+			reader.readAsText(file);
+		}
+	}
+
+	function handleTextChange() {
+		loadFile.value = '';
 	}
 </script>
 
@@ -13,7 +40,11 @@
 			browser data, change your device or browser, or you simply want to play on your computer at work and your
 			phone in the weekends. This is where saving manually comes in handy.
 		</p>
-		<p>If you'd like to play on more than one device, simply change your save file.</p>
+		<p class="italic text-sm">
+			Tip: Make a backup save every so often, especially when you've made progress you don't want to lose. Upload
+			your save file to an online cloud storage or a chatting application, or anywhere accessible to you from
+			several devices.
+		</p>
 	</div>
 	<button
 		class="border border-black p-2 cursor-copy"
@@ -36,7 +67,7 @@
 			const blob = new Blob([JSON.stringify($gameState)], { type: 'text/plain' });
 			const link = document.createElement('a');
 			link.href = URL.createObjectURL(blob);
-			link.download = 'seedclicker2_save.txt';
+			link.download = 'save.seed'; //filename
 			document.body.appendChild(link);
 			link.click();
 			document.body.removeChild(link);
@@ -45,16 +76,29 @@
 		Save as a text file
 	</button>
 	<br />
-	<button
-		class="border border-black p-2"
-		on:click={() => {
-			//TODO open textinput field
-			alert('not implemented');
-		}}
-	>
-		Load
-	</button>
-	<br />
+	<div id="load" class="m-4 p-2 border border-black">
+		<p>Load by pasting your save into the text field, or simply upload the file.</p>
+		<input type="file" id="loadfile" bind:this={loadFile} on:change={handleFileChange} />
+		<br />
+		<textarea
+			id="load_input"
+			placeholder="Save file"
+			bind:this={loadText}
+			on:change={handleTextChange}
+			class="resize my-4"
+		/>
+		<br />
+		<button
+			class="border border-black p-2"
+			on:click={() => {
+				const save = JSON.parse(loadText.value);
+				console.log('loading save', loadText.value, save);
+				alert('not implemented');
+			}}
+		>
+			Load
+		</button>
+	</div>
 	<button
 		class="border border-black p-2 bg-red-600"
 		on:click={() => {
