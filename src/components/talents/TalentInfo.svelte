0a3baba224e14ed6ff874b0700eb541f931bd37d
gummangummangumman <gumman@gumman.one>
2024-12-01 17:27:32 +0100

    border on buy talent button

--- src/components/talents/TalentInfo.svelte
+++ src/components/talents/TalentInfo.svelte
@@ -14,7 +14,9 @@
 	<h1>{talent.name}</h1>
 	<p class="font-bo">{talent.description}</p>
 	{#if canBuy(talent)}
-		<Button onclick={() => buyTalent(talent)} class="bg-primary p-2 px-4 mt-3 rounded-md">Buy</Button>
+		<Button onclick={() => buyTalent(talent)} class="bg-primary p-2 px-4 mt-3 rounded-md border border-secondary">
+			Buy
+		</Button>
 	{:else if talentOwned(talent)}
 		<p class="pt-3 text-lg">✅</p>
 	{:else if !requirementFulfilled(talent)}
